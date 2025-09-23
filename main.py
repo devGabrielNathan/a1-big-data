e320b52e68f7970eafd6acee4f7dcac2fc388eed
itallovidal <itallovidalll@gmail.com>
2025-09-23 17:27:09 -0300

    basic setup

---
+++ main.py
@@ -0,0 +1,12 @@
+# import sys
+import matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd
+matplotlib.use("agg")
+from utils.import_csv import import_csv
+
+csv = import_csv()
+
+
+
+
---
+++ utils/import_csv.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def import_csv():
+    print("Importando CSV")
+    try: 
+        csv = pd.read_csv("ITBI/dataset.csv")
+        print("CSV Importado com sucesso.")
+        return csv
+
+    except Exception as e:
+        print("Ocorreu um erro ao importar o csv.", e)
+
