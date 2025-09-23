792410d0f9facca4c3913dcc627613bdd874d040
Gabriel Nathan <devgabrielnathan@gmail.com>
2025-09-23 16:35:20 -0300

    initial data

---
+++ x.py
@@ -0,0 +1,11 @@
+# import sys
+import matplotlib
+import matplotlib.pyplot as plt
+
+matplotlib.use("agg")
+
+values = [1,2,3,4,5,6,7,8,9,10]
+
+plt.plot(values)
+plt.show()
+# plt.savefig(sys.stdout.buffer)
